8f0ebde60ce86e387e4f3b69284e6266ff5c84e3
sehaden540 <sgalloway2010@gmail.com>
2021-09-04 22:49:38 -0400

    init commit

--- GoLive.py
+++ GoLive.py
@@ -8,20 +8,16 @@ def authorize():
         'grant_type': 'client_credentials',
     }
 
-    print(token_params['client_id'])
     app_token_request = requests.post(
         'https://id.twitch.tv/oauth2/token', params=token_params)
-    print(app_token_request.content)
 
     return app_token_request.json()
 
 def main():
     twitch_app_token_json = authorize()
     access_token = twitch_app_token_json['access_token']
-    print('access token: ' + access_token)
     twitch_headers = {
         'Client-ID': os.getenv('twitch_client_id'),
         'Authorization': 'Bearer ' + access_token,
     }
-    requests.get('https://id.twitch.tv/oauth2/validate',
-                 headers=twitch_headers)
+    print('Twitch API Authenticated')
