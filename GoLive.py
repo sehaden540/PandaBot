e8453b6100bd35a68ab62dd5a92831e9453d4143
sehaden540 <sgalloway2010@gmail.com>
2021-09-05 14:58:15 -0400

    init commit

--- GoLive.py
+++ GoLive.py
@@ -1,7 +1,19 @@
 import requests
 import os
+import discord
 
-def authorize():
+discord_client = discord.Client()
+
+def authorize_discord():
+    TOKEN = os.getenv('discord_token')
+    discord_client.run(TOKEN)
+
+@discord_client.event
+async def on_ready():
+    print(f'{discord_client.user} has connected to Discord!')
+    await discord_client.close()
+
+def authorize_twitch():
     token_params = {
         'client_id': os.getenv('twitch_client_id'),
         'client_secret': os.getenv('twitch_client_secret'),
@@ -14,10 +26,12 @@ def authorize():
     return app_token_request.json()
 
 def main():
-    twitch_app_token_json = authorize()
+    twitch_app_token_json = authorize_twitch()
     access_token = twitch_app_token_json['access_token']
     twitch_headers = {
         'Client-ID': os.getenv('twitch_client_id'),
         'Authorization': 'Bearer ' + access_token,
     }
     print('Twitch API Authenticated')
+
+    authorize_discord()
